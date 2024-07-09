db56e7b0aa9cf69ba4f190459dbb4a4245a7d829
Fancy <fancy.wu@siliconmotion.com>
2024-07-09 17:11:10 +0800

    调整setOption设置，增加rtpFlag标识

--- app/src/main/java/cc/ralee/filterplayer/MediaPlayerWithSurface.java
+++ app/src/main/java/cc/ralee/filterplayer/MediaPlayerWithSurface.java
@@ -43,8 +43,10 @@ public class MediaPlayerWithSurface implements  IjkMediaPlayer.OnPreparedListene
         Log.d("Moive File", "videoPath:" + videoPath);
         Log.d("Moive File", "Exists:" + file.exists());
         Log.d("Moive File", "filePath:" + file.getPath());
-
-
+        boolean rtpFlag = false;
+        if (videoPath.startsWith("rtp")) {
+            rtpFlag = true;
+        }
         try {
             if(file.exists()) {
                 mediaPlayer.setDataSource(file.getPath());
@@ -58,56 +60,55 @@ public class MediaPlayerWithSurface implements  IjkMediaPlayer.OnPreparedListene
 
 
 
-            if (true) {
-                // Param for living
+            if (rtpFlag) {
+                // Param for rtp-living
                 mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "max_cached_duration", 3000);
                 mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "infbuf", 1);
                 mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "packet-buffering", 0);
+                //设置丢帧阈值
+                mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 1);
+                //设置无 packet 缓存
+                mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "fflags", "nobuffer");
+                // 预加载
+                mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "start-on-prepared", 1);//实时性
+                // 设置探测包数量
+                mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", 1024);// 该参数调小可解决卡顿问题以及起播时长问题 但是过小会导致起播失败需多次起播
+                // 设置分析流时长
+                mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzemaxduration", 10000); //调小可减短起播时长
+                //启用快速播放模式 (减少启动时间，降低播放延迟)
+                mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "fast", 1);
+
+
             } else {
                 // Param for playback
                 mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "max_cached_duration", 0);
                 mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "infbuf", 0);
                 mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "packet-buffering", 1);
+
             }
 
             //开启硬编码
-//            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec", 1);
-//            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-auto-rotate", 1);
-//            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-hevc", 1);
-//            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-handle-resolution-change", 1);
-
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec", 1);
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-auto-rotate", 1);
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-hevc", 1);
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "mediacodec-handle-resolution-change", 1);
+            // 启用HTTP服务器范围请求检测
             mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "http-detect-range-support", 1);
-
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "fast", 1);//不额外优化
-
-            // 设置丢帧阈值
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "framedrop", 1);
+            // 启用数据包刷新
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "flush_packets", 1L);
             // 设置视频帧率
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "fps", 15);
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "fps", 30);
             mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "max-fps", 30);
+            //指定视频输出的像素格式
             mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "overlay-format", IjkMediaPlayer.SDL_FCC_YV12);
-
             // 设置环路滤波
             mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_CODEC, "skip_loop_filter", 0);//48快但质量差，0慢但质量好
-
-            // 设置无 packet 缓存
+            // 设置缓冲区大小
             mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "buffer_size", 1024);
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "fflags", "nobuffer");
-            // 不限制拉流缓存大小
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "infbuf", "1");
             // 设置最大缓存数量
             mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "max-buffer-size", 1024*1024);
             // 设置最小解码帧数
             mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "min-frames", 3);
-            // 预加载
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "start-on-prepared", 1);//实时性
-            // 设置探测包数量
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "probesize", 1024);// 该参数调小可解决卡顿问题以及起播时长问题 但是过小会导致起播失败需多次起播
-            // 设置分析流时长
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzeduration", 10000);//起播问题相关
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "analyzemaxduration", 10000);
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "flush_packets", 1L);
-
 
             //设置音视频解码器格式格式
             //audio
@@ -120,7 +121,11 @@ public class MediaPlayerWithSurface implements  IjkMediaPlayer.OnPreparedListene
 //            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_CODEC, "pixel_format", 0);
 //            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_CODEC, "video_size", 1920); //{"video_size", "set video size", OFFSET(width), AV_OPT_TYPE_IMAGE_SIZE, {.str=NULL}, 0, INT_MAX, 0 }
 
+            //设置可循环播放
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "seek_at_start", 1);
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "reconnect", 1);
 
+            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT,"safe",0);
 
             //静音设置
 //            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "an", 1);
@@ -128,11 +133,10 @@ public class MediaPlayerWithSurface implements  IjkMediaPlayer.OnPreparedListene
 //            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "rtsp_transport", "tcp");
 
 //            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "protocol_whitelist", "crypto,file,http,https,tcp,tls,udp,rtp,rtsp");
-//            // 清空DNS,有时因为在APP里面要播放多种类型的视频(如:MP4,直播,直播平台保存的视频,和其他http视频), 有时会造成因为DNS的问题而报10000问题
+
+            // 清空DNS,有时因为在APP里面要播放多种类型的视频(如:MP4,直播,直播平台保存的视频,和其他http视频), 有时会造成因为DNS的问题而报10000问题
 //            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "dns_cache_clear", 1);
-//
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER, "seek_at_start", 1);
-            mediaPlayer.setOption(IjkMediaPlayer.OPT_CATEGORY_FORMAT, "reconnect", 1);
+
             mediaPlayer.prepareAsync();
         } catch (IOException e) {
             e.printStackTrace();
@@ -148,7 +152,9 @@ public class MediaPlayerWithSurface implements  IjkMediaPlayer.OnPreparedListene
         isPlaying = true;
         seekBar.setProgress(0);
         seekBar.setMax((int)mediaPlayer.getDuration());
+        Log.d("Player", "onprepared!! ");
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 if (progress >= 0) {
